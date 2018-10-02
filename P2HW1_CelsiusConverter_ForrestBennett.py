181c92085bdf1598477fa80c380c465fe4916972
LogikPro23 <43175213+LogikPro23@users.noreply.github.com>
2018-10-02 10:06:07 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_ForrestBennett.py
@@ -0,0 +1,13 @@
+# Celsius Fahrenheit Converter 
+# 09/06/18
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter 
+# Forrest Bennett
+# Get the temperature in Celsius.
+
+Temperature_in_celsius= float(input('Enter temperature in celsius'))
+
+# Convert Celsius to Fahrenheit.
+Fahrenheit= 1.8 * Temperature_in_celsius +32
+
+# Display the Temperature in Fahrenheit.
+print(Temperature_in_celsius, 'celsius is equal to', Fahrenheit, 'Fahrenheit')
